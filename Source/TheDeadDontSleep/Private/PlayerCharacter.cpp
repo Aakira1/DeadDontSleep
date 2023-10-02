286d093f13590b5c9d4eb519b8424efffc33f22f
Ayden Beggs <aydenbeggs@gmail.com>
2023-10-02 20:00:30 +1000

    Procedural Small Demo Added - Ayden

--- Source/TheDeadDontSleep/Private/PlayerCharacter.cpp
+++ Source/TheDeadDontSleep/Private/PlayerCharacter.cpp
@@ -1,16 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerCharacter.h"
 
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
-	
-
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
 	//Take Control of the default player
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
