938e963d7d49bb47c63b2294275233e93cfa4af0
Ayden Beggs <aydenbeggs@gmail.com>
2023-11-04 02:06:28 +1000

    Fix - Ayden

--- Source/TheDeadDontSleep/Private/Character/Abilities/GDDamageExecCalc.cpp
+++ Source/TheDeadDontSleep/Private/Character/Abilities/GDDamageExecCalc.cpp
@@ -7,8 +7,8 @@
 // Declare the attributes to capture and define how we want to capture them from the Source and Target.
 struct GDDamageStatics
 {
-	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
-	DECLARE_ATTRIBUTE_CAPTUREDEF(Damage);
+	//DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	//DECLARE_ATTRIBUTE_CAPTUREDEF(Damage);
 
 	GDDamageStatics()
 	{
@@ -17,10 +17,10 @@ struct GDDamageStatics
 		// We're not capturing anything from the Source in this example, but there could be like AttackPower attributes that you might want.
 
 		// Capture optional Damage set on the damage GE as a CalculationModifier under the ExecutionCalculation
-		DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSetBase, Damage, Source, true);
+		//DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSetBase, Damage, Source, true);
 
 		// Capture the Target's Armor. Don't snapshot.
-		DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSetBase, Armor, Target, false);
+		//DEFINE_ATTRIBUTE_CAPTUREDEF(UCharacterAttributeSetBase, Armor, Target, false);
 	}
 };
 
@@ -32,16 +32,12 @@ static const GDDamageStatics& DamageStatics()
 
 UGDDamageExecCalc::UGDDamageExecCalc()
 {
-	RelevantAttributesToCapture.Add(DamageStatics().DamageDef);
-	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	//RelevantAttributesToCapture.Add(DamageStatics().DamageDef);
+	//RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UGDDamageExecCalc::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecuteParams, OUT FGameplayEffectCustomExecutionOutput& OutExecOutput) const
 {
-	UAbilitySystemComponent* TargetAbilitySystemComponent = ExecuteParams.GetTargetAbilitySystemComponent();
-	UAbilitySystemComponent* SourceAbilitySystemComponent = ExecuteParams.GetSourceAbilitySystemComponent();
-
-	//AActor* SourceActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent.GetAvatarActor() : nullptr;
-	//AActor* TargetActor = TargetAbilitySystemComponent ? TargetAbilitySystemComponent->GetAvatarActor() : nullptr;
+	return;
 }
 
